bc2bea58c86b8089f150114ccc6721e3de03a929
cuyt <dphuc2304@gmail.com>
2024-07-17 03:28:43 +0700

    first commit

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Tiếng Trung Mộc Miên';
+export const SITE_DESCRIPTION = 'Trung tâm Tiếng Trung tại Đông Hà - Quảng Trị';
