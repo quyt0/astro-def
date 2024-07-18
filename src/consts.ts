b9d81b6d81f2bd46b4d2b61b91b521cd50cce3d4
cuyt <dphuc2304@gmail.com>
2024-07-18 10:54:16 +0700

    Up base 2

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Tiếng Trung Mộc Miên';
-export const SITE_DESCRIPTION = 'Trung tâm Tiếng Trung tại Đông Hà - Quảng Trị';
+export const SITE_DESCRIPTION = 'Với ngoại ngữ, mọi nơi đều là nhà';
+export const PHONE_NUMBER = '0233.3855.868';
+export const PHONE_NUMBER2 = '0819.260.345';
